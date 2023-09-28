d9db3d478a9450150f673baab9544d7babb50a45
SSFSKIM <kimmi@ssfs.org>
2023-09-29 00:16:06 +0900

    XFD

--- currency.py
+++ currency.py
@@ -1,4 +1,8 @@
+#Steve
+#23.09.28
 money = int(input('whats the money?'))
 print(f'{money} can be made with:')
-for a in ():
-    print(f'{money//100} hundred dollar bill(s){money//50} fifty dollar bill(s)'))
+left = 0
+for a in (100, 50, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01):
+    print(f'{money//a} a dollar bill(s)')
+    money = money%a
