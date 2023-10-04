5868c73a54bfeda4f366c07c58bc04636b449bc5
SSFSKIM <kimmi@ssfs.org>
2023-10-05 05:34:19 +0900

    cu

--- assignment_two.py
+++ assignment_two.py
@@ -2,6 +2,9 @@
 #23.09.29
 
 import turtle
+
+import sympy as sp
+
 import math
 #get input for 2 coordinates
 x1 = int(input('type the x coordinate of the first point: '))
@@ -12,18 +15,19 @@ y2 = int(input('type the y coordinate of the second point: '))
 turtle.goto(x1, y1)
 turtle.goto(0, 0)
 turtle.goto(x2, y2)
-slope1 = y1/x1
-slope2 = y2/x2
-#when one of x coordinate is 0, it gets error because of devision by zero, so solving by case
-if x1 == 0:
-    #when one of the coordinate is 0, 1/slope is tangent value
-    tan = 1/slope2
-elif x2 == 0:
-    tan = 1/slope1
-else:
-    tan = (slope2-slope1)/(1+slope1*slope2)
-#get angle value using arctangent function
-angle = abs(math.atan(tan)*180/math.pi)
+
+#define limit variables
+x_1 = sp.symbols('x_1')
+x_2 = sp.symbols('x_2')
+
+#get slope using limit so that it works even when input 0
+slope1 = sp.limit(y1/x_1, x_1, x1)
+slope2 = sp.limit(y2/x_2, x_2, x2)
+
+
+#get angle value using arctangent function, using limit to calculate even when division by zero happen
+
+angle = abs(sp.limit(math.atan(slope1), x_1, x1) - sp.limit(math.atan(slope2), x_1, x1)*180/math.pi)
 
 #go to 0,0 to write number
 turtle.penup()
